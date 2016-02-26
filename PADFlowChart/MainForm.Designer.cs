33e92e979b6fbb4fb4131362f02b21d39b361e29
semaluo <luojiaolong2015@hotmail.com>
2016-02-26 16:42:23 +0800

    Add winforms

--- PADFlowChart/MainForm.Designer.cs
+++ PADFlowChart/MainForm.Designer.cs
@@ -28,20 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            WeifenLuo.WinFormsUI.Docking.DockPanelSkin dockPanelSkin1 = new WeifenLuo.WinFormsUI.Docking.DockPanelSkin();
+            WeifenLuo.WinFormsUI.Docking.AutoHideStripSkin autoHideStripSkin1 = new WeifenLuo.WinFormsUI.Docking.AutoHideStripSkin();
+            WeifenLuo.WinFormsUI.Docking.DockPanelGradient dockPanelGradient1 = new WeifenLuo.WinFormsUI.Docking.DockPanelGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient1 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.DockPaneStripSkin dockPaneStripSkin1 = new WeifenLuo.WinFormsUI.Docking.DockPaneStripSkin();
+            WeifenLuo.WinFormsUI.Docking.DockPaneStripGradient dockPaneStripGradient1 = new WeifenLuo.WinFormsUI.Docking.DockPaneStripGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient2 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.DockPanelGradient dockPanelGradient2 = new WeifenLuo.WinFormsUI.Docking.DockPanelGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient3 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.DockPaneStripToolWindowGradient dockPaneStripToolWindowGradient1 = new WeifenLuo.WinFormsUI.Docking.DockPaneStripToolWindowGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient4 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient5 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.DockPanelGradient dockPanelGradient3 = new WeifenLuo.WinFormsUI.Docking.DockPanelGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient6 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            WeifenLuo.WinFormsUI.Docking.TabGradient tabGradient7 = new WeifenLuo.WinFormsUI.Docking.TabGradient();
+            this.menuStrip_main = new System.Windows.Forms.MenuStrip();
+            this.menu_file = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_file_new = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_file_open = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_file_close = new System.Windows.Forms.ToolStripMenuItem();
+            this.全部保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_file_saveAsPicture = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_edit_copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_edit_paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_edit_selectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolbar_open = new System.Windows.Forms.ToolStripButton();
+            this.toolbar_save = new System.Windows.Forms.ToolStripButton();
+            this.dockPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.toolbar_saveAll = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip_main.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip_main
+            // 
+            this.menuStrip_main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_file,
+            this.menu_edit});
+            this.menuStrip_main.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip_main.Name = "menuStrip_main";
+            this.menuStrip_main.Size = new System.Drawing.Size(584, 25);
+            this.menuStrip_main.TabIndex = 0;
+            // 
+            // menu_file
+            // 
+            this.menu_file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_file_new,
+            this.menu_file_open,
+            this.menu_file_close,
+            this.全部保存ToolStripMenuItem,
+            this.menu_file_saveAsPicture});
+            this.menu_file.Name = "menu_file";
+            this.menu_file.Size = new System.Drawing.Size(44, 21);
+            this.menu_file.Text = "文件";
+            // 
+            // menu_file_new
+            // 
+            this.menu_file_new.Name = "menu_file_new";
+            this.menu_file_new.Size = new System.Drawing.Size(152, 22);
+            this.menu_file_new.Text = "新建";
+            // 
+            // menu_file_open
+            // 
+            this.menu_file_open.Name = "menu_file_open";
+            this.menu_file_open.Size = new System.Drawing.Size(152, 22);
+            this.menu_file_open.Text = "打开";
+            // 
+            // menu_file_close
+            // 
+            this.menu_file_close.Name = "menu_file_close";
+            this.menu_file_close.Size = new System.Drawing.Size(152, 22);
+            this.menu_file_close.Text = "关闭";
+            // 
+            // 全部保存ToolStripMenuItem
+            // 
+            this.全部保存ToolStripMenuItem.Name = "全部保存ToolStripMenuItem";
+            this.全部保存ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.全部保存ToolStripMenuItem.Text = "全部保存";
+            // 
+            // menu_file_saveAsPicture
+            // 
+            this.menu_file_saveAsPicture.Name = "menu_file_saveAsPicture";
+            this.menu_file_saveAsPicture.Size = new System.Drawing.Size(152, 22);
+            this.menu_file_saveAsPicture.Text = "另存为图片";
+            // 
+            // menu_edit
+            // 
+            this.menu_edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_edit_copy,
+            this.menu_edit_paste,
+            this.menu_edit_selectAll});
+            this.menu_edit.Name = "menu_edit";
+            this.menu_edit.Size = new System.Drawing.Size(44, 21);
+            this.menu_edit.Text = "编辑";
+            // 
+            // menu_edit_copy
+            // 
+            this.menu_edit_copy.Name = "menu_edit_copy";
+            this.menu_edit_copy.Size = new System.Drawing.Size(152, 22);
+            this.menu_edit_copy.Text = "复制";
+            // 
+            // menu_edit_paste
+            // 
+            this.menu_edit_paste.Name = "menu_edit_paste";
+            this.menu_edit_paste.Size = new System.Drawing.Size(152, 22);
+            this.menu_edit_paste.Text = "粘贴";
+            // 
+            // menu_edit_selectAll
+            // 
+            this.menu_edit_selectAll.Name = "menu_edit_selectAll";
+            this.menu_edit_selectAll.Size = new System.Drawing.Size(152, 22);
+            this.menu_edit_selectAll.Text = "全选";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolbar_open,
+            this.toolbar_save,
+            this.toolbar_saveAll});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(584, 26);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolbar_open
+            // 
+            this.toolbar_open.AutoSize = false;
+            this.toolbar_open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbar_open.Image = ((System.Drawing.Image)(resources.GetObject("toolbar_open.Image")));
+            this.toolbar_open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbar_open.Name = "toolbar_open";
+            this.toolbar_open.Size = new System.Drawing.Size(23, 23);
+            this.toolbar_open.Text = "Open";
+            // 
+            // toolbar_save
+            // 
+            this.toolbar_save.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbar_save.Image = ((System.Drawing.Image)(resources.GetObject("toolbar_save.Image")));
+            this.toolbar_save.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbar_save.Name = "toolbar_save";
+            this.toolbar_save.Size = new System.Drawing.Size(23, 23);
+            this.toolbar_save.Text = "Save";
+            // 
+            // dockPanel
+            // 
+            this.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanel.Location = new System.Drawing.Point(0, 51);
+            this.dockPanel.Name = "dockPanel";
+            this.dockPanel.Size = new System.Drawing.Size(584, 363);
+            dockPanelGradient1.EndColor = System.Drawing.SystemColors.ControlLight;
+            dockPanelGradient1.StartColor = System.Drawing.SystemColors.ControlLight;
+            autoHideStripSkin1.DockStripGradient = dockPanelGradient1;
+            tabGradient1.EndColor = System.Drawing.SystemColors.Control;
+            tabGradient1.StartColor = System.Drawing.SystemColors.Control;
+            tabGradient1.TextColor = System.Drawing.SystemColors.ControlDarkDark;
+            autoHideStripSkin1.TabGradient = tabGradient1;
+            autoHideStripSkin1.TextFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            dockPanelSkin1.AutoHideStripSkin = autoHideStripSkin1;
+            tabGradient2.EndColor = System.Drawing.SystemColors.ControlLightLight;
+            tabGradient2.StartColor = System.Drawing.SystemColors.ControlLightLight;
+            tabGradient2.TextColor = System.Drawing.SystemColors.ControlText;
+            dockPaneStripGradient1.ActiveTabGradient = tabGradient2;
+            dockPanelGradient2.EndColor = System.Drawing.SystemColors.Control;
+            dockPanelGradient2.StartColor = System.Drawing.SystemColors.Control;
+            dockPaneStripGradient1.DockStripGradient = dockPanelGradient2;
+            tabGradient3.EndColor = System.Drawing.SystemColors.ControlLight;
+            tabGradient3.StartColor = System.Drawing.SystemColors.ControlLight;
+            tabGradient3.TextColor = System.Drawing.SystemColors.ControlText;
+            dockPaneStripGradient1.InactiveTabGradient = tabGradient3;
+            dockPaneStripSkin1.DocumentGradient = dockPaneStripGradient1;
+            dockPaneStripSkin1.TextFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            tabGradient4.EndColor = System.Drawing.SystemColors.ActiveCaption;
+            tabGradient4.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            tabGradient4.StartColor = System.Drawing.SystemColors.GradientActiveCaption;
+            tabGradient4.TextColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dockPaneStripToolWindowGradient1.ActiveCaptionGradient = tabGradient4;
+            tabGradient5.EndColor = System.Drawing.SystemColors.Control;
+            tabGradient5.StartColor = System.Drawing.SystemColors.Control;
+            tabGradient5.TextColor = System.Drawing.SystemColors.ControlText;
+            dockPaneStripToolWindowGradient1.ActiveTabGradient = tabGradient5;
+            dockPanelGradient3.EndColor = System.Drawing.SystemColors.ControlLight;
+            dockPanelGradient3.StartColor = System.Drawing.SystemColors.ControlLight;
+            dockPaneStripToolWindowGradient1.DockStripGradient = dockPanelGradient3;
+            tabGradient6.EndColor = System.Drawing.SystemColors.InactiveCaption;
+            tabGradient6.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            tabGradient6.StartColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            tabGradient6.TextColor = System.Drawing.SystemColors.InactiveCaptionText;
+            dockPaneStripToolWindowGradient1.InactiveCaptionGradient = tabGradient6;
+            tabGradient7.EndColor = System.Drawing.Color.Transparent;
+            tabGradient7.StartColor = System.Drawing.Color.Transparent;
+            tabGradient7.TextColor = System.Drawing.SystemColors.ControlDarkDark;
+            dockPaneStripToolWindowGradient1.InactiveTabGradient = tabGradient7;
+            dockPaneStripSkin1.ToolWindowGradient = dockPaneStripToolWindowGradient1;
+            dockPanelSkin1.DockPaneStripSkin = dockPaneStripSkin1;
+            this.dockPanel.Skin = dockPanelSkin1;
+            this.dockPanel.TabIndex = 2;
+            // 
+            // toolbar_saveAll
+            // 
+            this.toolbar_saveAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolbar_saveAll.Image = ((System.Drawing.Image)(resources.GetObject("toolbar_saveAll.Image")));
+            this.toolbar_saveAll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbar_saveAll.Name = "toolbar_saveAll";
+            this.toolbar_saveAll.Size = new System.Drawing.Size(23, 23);
+            this.toolbar_saveAll.Text = "SaveAll";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 414);
+            this.Controls.Add(this.dockPanel);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip_main);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip_main;
             this.Name = "MainForm";
             this.Text = "PAD流程图";
+            this.menuStrip_main.ResumeLayout(false);
+            this.menuStrip_main.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip_main;
+        private System.Windows.Forms.ToolStripMenuItem menu_file;
+        private System.Windows.Forms.ToolStripMenuItem menu_file_new;
+        private System.Windows.Forms.ToolStripMenuItem menu_file_open;
+        private System.Windows.Forms.ToolStripMenuItem menu_file_close;
+        private System.Windows.Forms.ToolStripMenuItem 全部保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu_file_saveAsPicture;
+        private System.Windows.Forms.ToolStripMenuItem menu_edit;
+        private System.Windows.Forms.ToolStripMenuItem menu_edit_copy;
+        private System.Windows.Forms.ToolStripMenuItem menu_edit_paste;
+        private System.Windows.Forms.ToolStripMenuItem menu_edit_selectAll;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolbar_open;
+        private System.Windows.Forms.ToolStripButton toolbar_save;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel;
+        private System.Windows.Forms.ToolStripButton toolbar_saveAll;
     }
 }
 
